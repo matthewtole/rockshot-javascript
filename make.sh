d6ef4dc67b5e9245727ce5a263a15e0591ecb3f0
Matthew Tole <matthewtole@gmail.com>
2013-11-09 15:47:08 +0000

    Remove unused JS library and modify build script.

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 jshint src/rockshot.js || { exit 1; }
-uglifyjs src/superagent.js src/firebase.js src/rockshot.js -o rockshot.min.js || { exit 1; }
+uglifyjs src/rockshot.js -o rockshot.min.js || { exit 1; }
+uglifyjs src/firebase.js src/rockshot.js -o rockshot-with-firebase.min.js || { exit 1; }
